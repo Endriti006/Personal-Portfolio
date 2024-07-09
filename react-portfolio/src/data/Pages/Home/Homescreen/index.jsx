ae2150426ec965d4ebb29fb5166958a10710a151
Endriti Kryeziu <endriti006@gmail.com>
2024-07-09 17:59:24 +0200

    a

--- react-portfolio/src/data/Pages/Home/Homescreen/index.jsx
+++ react-portfolio/src/data/Pages/Home/Homescreen/index.jsx
@@ -1,11 +1,17 @@
 import HeroSection from "../HeroSeaction";
 import MySkills from "../MySkills";
+import AboutMe from "../AboutMe";
+import MyPortfolio from "../MyPortfolio";
+import Testimonial from "../Testimonial";
 
 export default function Home(){
     return(
         <>
         <HeroSection />
         <MySkills />
+        <AboutMe/>
+        <MyPortfolio/>
+        <Testimonial/>
         </>
     )
 }
