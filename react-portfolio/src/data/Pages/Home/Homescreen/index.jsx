86fc82df3f6e5be68cb874e951ef65179f956fdf
Endriti Kryeziu <endriti006@gmail.com>
2024-07-19 00:27:38 +0200

    Update index.jsx

--- react-portfolio/src/data/Pages/Home/Homescreen/index.jsx
+++ react-portfolio/src/data/Pages/Home/Homescreen/index.jsx
@@ -3,6 +3,8 @@ import MySkills from "../MySkills";
 import AboutMe from "../AboutMe";
 import MyPortfolio from "../MyPortfolio";
 import Testimonial from "../Testimonial";
+import ContanctMe from "../ContactMe";
+import Footer from "../Footer";
 
 export default function Home(){
     return(
@@ -12,6 +14,8 @@ export default function Home(){
         <AboutMe/>
         <MyPortfolio/>
         <Testimonial/>
+        <ContanctMe/>
+        <Footer/>
         </>
     )
 }
